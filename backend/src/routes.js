5b89be7b336acebd0df1cdbd79baa75f9702258a
genildo2002 <genildo2002@gmail.com>
2020-03-29 21:26:09 -0300

    Mobile - novos

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const { celebrate, Joi, Segments } = require('celebrate');
+
 const OngController = require('./controllers/OngController')
 const IncidentController = require('./controllers/IncidentController')
 const ProfilerController = require('./controllers/ProfilerController')
 const SessionController = require('./controllers/SessionController')
+
+
 //const crypto = require('crypto');
 
 //const connection = require('./database/connection.js');
@@ -14,13 +18,13 @@ const routes = express.Router();
 //     const ongs = await connection('ongs').select('*');
 //     return response.json(ongs);
 // })
- 
+
 
 // Criar Ong
  // Foi tudo isso para o controller
 //routes.post('/ongs', async(request,response)=> {
     // Foi tudo isso para o controller
-    // const { name, email, whatsup, city, uf } = request.body;      // Acessando os route params    
+    // const { name, email, whatsup, city, uf } = request.body;      // Acessando os route params
     // const id = crypto.randomBytes(4).toString('HEX');
     // console.log(id);                // http://localhost:3333/users/1
     // await connection('ongs').insert({
@@ -40,14 +44,36 @@ const routes = express.Router();
 routes.post('/sessions',SessionController.create);
 //*********************************************************
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.create);
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsup: Joi.string().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    }),
+}),OngController.create);
 routes.delete('/ongs/:id', OngController.delete);
 //*********************************************************
-routes.get('/incidents',IncidentController.index);
+routes.get('/incidents',celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number()
+    }),
+}),IncidentController.index);
+
 routes.post('/incidents',IncidentController.create);
-routes.delete('/incidents/:id',IncidentController.delete);
+
+routes.delete('/incidents/:id',celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    })
+}),IncidentController.delete);
 //*********************************************************
-routes.get('/profiler',ProfilerController.index);
+routes.get('/profiler',celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+}),ProfilerController.index);
 
 module.exports = routes;
 
@@ -61,12 +87,12 @@ module.exports = routes;
 
 /*
 * Typos de parâmetros:
-* Query Params : Parâmetros enviados narota apos [?], servem para filtros, paginação
+* Query Params : Parâmetros enviados na rota apos [?], servem para filtros, paginação
 * ex: http://loclahost:3333/users?[name=genildo]
 * Route params : Parâmetros utilizados pra identificar recursos
 * ex: app.get('/users/:id',(request,response)=> {
 *     http://loclahost:3333/users/1
-* Request Body: Corpo darequisição
+* Request Body: Corpo da requisição
 */
 
 /*
